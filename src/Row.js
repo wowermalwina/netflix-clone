ece563ed6b97758a775382383c2dc36b00e9e259
wowermalwina <wowermalwina@gmail.com>
2022-01-03 22:19:43 +0000

    styled rows

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -19,9 +20,13 @@ function Row({ title, fetchUrl }) {
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="rowPosters">
+      <div className="row_posters">
         {movies.map((movie) => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
